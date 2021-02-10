e1b241bba9516275f3a67721e20beec4b81ba0a2
Abdellah <mounajaikabdellah@gmail.com>
2021-02-10 12:23:24 +0100

    reponsive_fini_debut_js

---
+++ public/js/main.js
@@ -0,0 +1,67 @@
+let body = document.querySelector('body')
+
+let noir = document.querySelector(".noir")
+
+let blanc = document.querySelector(".blanc")
+
+let li = document.querySelector("li")
+let li2 = document.querySelectorAll("li")[1]
+let li3 = document.querySelectorAll("li")[2]
+let li4 = document.querySelectorAll("li")[3]
+let li5 = document.querySelectorAll("li")[4]
+let li6 = document.querySelectorAll("li")[5]
+let li7 = document.querySelectorAll("li")[6]
+let t2 = document.querySelector('.t2')
+let t1p = document.querySelector('.t1p')
+
+noir.addEventListener('click',()=>{
+    body.style.backgroundColor = 'black'
+    body.style.color = "black"
+    li.style.color = "black"
+    li2.style.color = "black"
+    li3.style.color = "black"
+    li4.style.color = "black"
+    li5.style.color = "black"
+    li6.style.color = "black"
+    li7.style.color = "black"
+    t2.style.color = "black"
+    t1p.style.color = "black"
+    header.style.color = "black"
+})
+
+blanc.addEventListener('click',()=>{
+    body.style.backgroundColor = "white"
+    body.style.color = "black"
+    li.style.color = "black"
+    li2.style.color = "black"
+    li3.style.color = "black"
+    li4.style.color = "black"
+    li5.style.color = "black"
+    li6.style.color = "black"
+    li7.style.color = "black"
+    header.style.zIndex = "1"
+})
+
+let header = document.querySelector('.header')
+let h1 = document.querySelectorAll('h1')[1]
+let vis = document.querySelector('.cacher')
+window.addEventListener("scroll",()=>{
+    if (window.scrollY > 100) {
+        vis.className = ("invisible","visible")
+    } else {
+        
+    }
+    if (window.scrollY > 100) {
+        h1.className = ("visible","invisible")
+    }
+})
+
+let connexion = document.querySelector("#connexion")
+
+connexion.addEventListener('')
+
+
+
+
+
+    
